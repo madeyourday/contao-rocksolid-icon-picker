182669382d4f2b76769113f7dc23d2125a83d0fd
Martin Auswöger <martin@auswoeger.com>
2017-07-25 14:31:14 +0200

    Code style

--- src/Widget/IconPicker.php
+++ src/Widget/IconPicker.php
@@ -64,12 +64,12 @@ class IconPicker extends \Widget
 			}
 		}
 
-        if (!$this->mandatory) {
-            $icons = array_merge(
-                array(array('code' => '', 'name' => 'none')),
-                $icons
-            );
-        }
+		if (!$this->mandatory) {
+			$icons = array_merge(
+				array(array('code' => '', 'name' => 'none')),
+				$icons
+			);
+		}
 
 		$html .= '<div class="rip_icons rip_collapsed" id="rip_icons_' . $this->strId . '" style="font-family: ' . $fontFamily . ';">';
 		$html .= '<div class="rip_icons_toolbar">';
